857beec0b14b76def41aab370b3ec058cf02b07a
horita-yuya <horitayuya@gmail.com>
2024-11-21 22:13:28 +0900

    Add mahjong-api

--- samples/mahjong-api/README.md
+++ samples/mahjong-api/README.md
@@ -1,39 +1,9 @@
-```typescript
-type RonChildCase<
-  HAND extends Hand,
-  WINNING extends HAND[number],
-  NAKI extends HAND[number][],
-> = MahjongKun<HAND, WINNING, NAKI, "ron", "child">;
-
-type Case1_1_2 = ExpectTrue<
-  Equal<
-    RonChildCase<
-      [
-        "o2",
-        "o3",
-        "o4",
-        "I2",
-        "I2",
-        "I2",
-        "C4",
-        "C5",
-        "C6",
-        "I6",
-        "I6",
-        "I6",
-        "o4",
-        "o4",
-      ],
-      "o3",
-      []
-    >,
-    "1300"
-  >
->;
+```shell
+yarn build
+yarn zip
+yarn deploy
 ```
 
 ```shell
 aws lambda invoke --function-name tlrt-sample --payload '{"t1": "o2", "t2": "o3", "t3": "o4", "t4": "I2", "t5": "I2", "t6": "I2", "t7": "C4", "t8": "C5", "t9": "C6", "t10": "I6", "t11": "I6", "t12": "I6", "t13": "o4", "t14": "o4", "winning": "o3", "style": "ron", "position": "child"}' --cli-binary-format raw-in-base64-out /tmp/res.json && cat /tmp/res.json
 ```
-
-'{"t1": "o2", "t2": "o3", "t3": "o4", "t4": "I2", "t5": "I2", "t6": "I2", "t7": "C4", "t8": "C5", "t9": "C6", "t10": "I6", "t11": "I6", "t12": "I6", "t13": "o4", "t14": "o4", "winning": "o3", "style": "ron", "position": "child"}'
--- samples/mahjong-api/index.ts
+++ samples/mahjong-api/index.ts
@@ -1,36 +1,44 @@
-import {MahjongKun, Tile} from "mahjong-kun";
+import type { MahjongKun, Tile } from "mahjong-kun";
 
-export type Handler<EVENTS extends {
-  t1: Tile;
-  t2: Tile;
-  t3: Tile;
-  t4: Tile;
-  t5: Tile;
-  t6: Tile;
-  t7: Tile;
-  t8: Tile;
-  t9: Tile;
-  t10: Tile;
-  t11: Tile;
-  t12: Tile;
-  t13: Tile;
-  t14: Tile;
-  winning: Tile;
-  style: "ron" | "tumo";
-  position: "parent" | "child";
-}> = MahjongKun<[
-  EVENTS["t1"],
-  EVENTS["t2"],
-  EVENTS["t3"],
-  EVENTS["t4"],
-  EVENTS["t5"],
-  EVENTS["t6"],
-  EVENTS["t7"],
-  EVENTS["t8"],
-  EVENTS["t9"],
-  EVENTS["t10"],
-  EVENTS["t11"],
-  EVENTS["t12"],
-  EVENTS["t13"],
-  EVENTS["t14"]
-], EVENTS["winning"], [], EVENTS["style"], EVENTS["position"]>
+export type Handler<
+  EVENTS extends {
+    t1: Tile;
+    t2: Tile;
+    t3: Tile;
+    t4: Tile;
+    t5: Tile;
+    t6: Tile;
+    t7: Tile;
+    t8: Tile;
+    t9: Tile;
+    t10: Tile;
+    t11: Tile;
+    t12: Tile;
+    t13: Tile;
+    t14: Tile;
+    winning: Tile;
+    style: "ron" | "tumo";
+    position: "parent" | "child";
+  },
+> = MahjongKun<
+  [
+    EVENTS["t1"],
+    EVENTS["t2"],
+    EVENTS["t3"],
+    EVENTS["t4"],
+    EVENTS["t5"],
+    EVENTS["t6"],
+    EVENTS["t7"],
+    EVENTS["t8"],
+    EVENTS["t9"],
+    EVENTS["t10"],
+    EVENTS["t11"],
+    EVENTS["t12"],
+    EVENTS["t13"],
+    EVENTS["t14"],
+  ],
+  EVENTS["winning"],
+  [],
+  EVENTS["style"],
+  EVENTS["position"]
+>;
